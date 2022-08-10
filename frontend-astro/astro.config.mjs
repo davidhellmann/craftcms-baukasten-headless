6eaa5ec943c030bb6b105d0c07e2009fd590571b
davidhellmann <david@hellmann.io>
2022-08-10 23:32:49 +0200

    🔧 config: update config

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -2,14 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import graphql from "@rollup/plugin-graphql";
 import cloudflare from "@astrojs/cloudflare";
+import { loadEnv } from "vite";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: cloudflare(),
-  site: "https://astro.live.baukaten.io",
-  vite: {
-    plugins: [graphql()],
-  },
+export default defineConfig((mode) => {
+  const { SITE } = loadEnv(mode, process.cwd(), "");
+  console.log(SITE);
+  return {
+    integrations: [tailwind()],
+    output: "server",
+    adapter: cloudflare(),
+    site: SITE,
+    vite: {
+      plugins: [graphql()],
+    },
+  };
 });

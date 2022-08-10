55d9a6904aa7f215423b690785f47133021d054f
davidhellmann <david@hellmann.io>
2022-08-10 23:58:09 +0200

    Static Test

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import graphql from "@rollup/plugin-graphql";
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 import { loadEnv } from "vite";
 
 // https://astro.build/config
@@ -10,8 +10,9 @@ export default defineConfig((mode) => {
   console.log(SITE);
   return {
     integrations: [tailwind()],
-    output: "server",
-    adapter: cloudflare(),
+    // output: "server",
+    output: "static",
+    // adapter: cloudflare(),
     site: SITE,
     vite: {
       plugins: [graphql()],

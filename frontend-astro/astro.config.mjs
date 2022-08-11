9efd005d693df4d8c808e29d2f7e127fb39c1d65
davidhellmann <david@hellmann.io>
2022-08-11 23:34:32 +0200

    🎨 chore: add live preview logic

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -7,13 +7,24 @@ import { loadEnv } from "vite";
 // https://astro.build/config
 export default defineConfig((mode) => {
   const { PUBLIC_SITE, PUBLIC_LIVE_PREVIEW } = loadEnv(mode, process.cwd(), "");
-  return {
+  const settings = {
     integrations: [tailwind()],
-    output: PUBLIC_LIVE_PREVIEW === "true" ? "server" : "static",
-    adapter: PUBLIC_LIVE_PREVIEW === "true" ? cloudflare() : {},
     site: PUBLIC_SITE,
     vite: {
       plugins: [graphql()],
     },
+    build: {
+      format: "file",
+    },
   };
+
+  // If Live Preview true do SSR
+  if (PUBLIC_LIVE_PREVIEW === "true") {
+    settings["adapter"] = cloudflare();
+    settings["output"] = "server";
+  } else {
+    settings["output"] = "static";
+  }
+
+  return settings;
 });

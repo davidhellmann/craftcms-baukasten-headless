22df4d3162d704582012384fa9aed803b6a72ce4
davidhellmann <david@hellmann.io>
2022-08-15 10:49:40 +0200

    fix web alias

--- backend-craftcms/web/index.php
+++ backend-craftcms/web/index.php
@@ -6,7 +6,6 @@
 // Set path constants
 define('CRAFT_BASE_PATH', dirname(__DIR__));
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH.'/vendor');
-define('CRAFT_CP', true);
 
 // Load Composer's autoloader
 require_once CRAFT_VENDOR_PATH.'/autoload.php';

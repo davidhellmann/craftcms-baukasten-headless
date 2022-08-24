61db414bce5fc404ed403ab5c5f04386c0222d3c
David Hellmann <david@hellmann.io>
2022-08-24 19:00:56 +0200

    🎨 chore: remove cypress

--- frontend-next/cypress/pages/index.spec.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { mount } from '@cypress/react'
-import IndexPage from '../../src/pages/index'
-
-it('Renders page component', () => {
-	mount(<IndexPage />)
-	cy.contains('Home page')
-})

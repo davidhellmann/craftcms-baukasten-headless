e8d6f6262d8d739be8311190339176dc888c8785
David Hellmann <david.hellmann@dynatrace.com>
2022-09-27 13:36:49 +0200

    🎨 chore: remove target: static from config

--- frontend-nuxt/nuxt.config.ts
+++ frontend-nuxt/nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
       nitroConfig.prerender.routes = [...routes]
     }
   },
-  target: 'static',
+  // target: 'static',
   router: {
     trailingSlash: false,
   },

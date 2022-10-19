b3dd43d350b2fd9e4c9aa2f526684277beebe5be
David Hellmann <david@hellmann.io>
2022-10-19 12:50:50 +0200

    fix: astro config

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -2,38 +2,60 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 import graphql from '@rollup/plugin-graphql';
-import cloudflare from '@astrojs/cloudflare';
+// import cloudflare from '@astrojs/cloudflare';
 import { loadEnv } from 'vite';
 
+process.env = { ...process.env, ...loadEnv('production', process.cwd()) };
+
 // https://astro.build/config
-export default defineConfig((mode) => {
-  const { PUBLIC_SITE, PUBLIC_LIVE_PREVIEW } = loadEnv(mode, process.cwd(), '');
-  const settings = {
-    integrations: [
-      tailwind({
-        config: {
-          path: './tailwind.config.js',
-          applyBaseStyles: false,
-        },
-      }),
-      svelte(),
-    ],
-    site: PUBLIC_SITE,
-    vite: {
-      plugins: [graphql()],
-    },
-    build: {
-      format: 'file',
-    },
-  };
-
-  // If Live Preview true do SSR
-  if (PUBLIC_LIVE_PREVIEW === 'true') {
-    settings['adapter'] = cloudflare();
-    settings['output'] = 'server';
-  } else {
-    settings['output'] = 'static';
-  }
-
-  return settings;
+export default defineConfig({
+  integrations: [
+    tailwind({
+      config: {
+        path: './tailwind.config.js',
+        applyBaseStyles: false,
+      },
+    }),
+    svelte(),
+  ],
+  site: process.env.PUBLIC_SITE,
+  vite: {
+    plugins: [graphql()],
+  },
+  build: {
+    format: 'file',
+  },
+  output: 'server',
 });
+
+// export default defineConfig((mode) => {
+//   const { PUBLIC_SITE, PUBLIC_LIVE_PREVIEW } = loadEnv(mode, process.cwd(), '');
+//   const settings = {
+//     integrations: [
+//       tailwind({
+//         config: {
+//           path: './tailwind.config.js',
+//           applyBaseStyles: false,
+//         },
+//       }),
+//       svelte(),
+//     ],
+//     site: PUBLIC_SITE,
+//     vite: {
+//       plugins: [graphql()],
+//     },
+//     build: {
+//       format: 'file',
+//     },
+//   };
+
+//   // If Live Preview true do SSR
+//   if (PUBLIC_LIVE_PREVIEW === 'true') {
+//     settings['adapter'] = cloudflare();
+//     settings['output'] = 'server';
+//   } else {
+//     settings['output'] = 'static';
+//   }
+
+//   return settings;
+// });

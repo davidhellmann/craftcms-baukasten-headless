b70e7e105053f9f6c059acb4bfa9453a9c20f276
David Hellmann <david@hellmann.io>
2022-11-16 20:46:50 +0100

    chore(astro): move composables to utils

--- frontend-astro/src/composables/useCapitalizeFirstLetter.ts
+++
@@ -1,7 +0,0 @@
-interface ICapitalizeFirstLetter {
-  string: string;
-}
-
-export const useCapitalizeFirstLetter = (params: ICapitalizeFirstLetter) => {
-  return params.string.charAt(0).toUpperCase() + params.string.slice(1);
-}

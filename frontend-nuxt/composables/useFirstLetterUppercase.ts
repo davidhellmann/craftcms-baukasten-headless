ebd537769cb06d260538b19127a9f40723c1e0f7
David Hellmann <david.hellmann@dynatrace.com>
2023-06-10 00:00:01 +0200

    remove: nuxt/next

--- frontend-nuxt/composables/useFirstLetterUppercase.ts
+++
@@ -1,8 +0,0 @@
-interface IFirstLetterToUppercase {
-  text: string,
-}
-
-export const useFirstLetterUppercase = (params: IFirstLetterToUppercase) => {
-  if (!params.text) return
-  return params.text.charAt(0).toUpperCase() + params.text.slice(1);
-}
